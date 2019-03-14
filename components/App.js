c4fb8d9ef4059aaf124166e346829012accac478
วงณัฏฐี เงาะปก <s6002041520107@MacMini-213-03.local>
2019-03-14 16:39:55 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,18 @@
+//import library
+import React from 'react';
+import { View, Text } from 'react-native';
+//wrtie component
+class App extends React.Component {
+    render() {
+        return(
+            //component about react native
+            <View>
+                <Text>Hello World!!</Text>
+            </View>
+        );
+}
+
+}
+
+//export
+export default App;
