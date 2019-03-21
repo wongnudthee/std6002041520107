e04d905316c4de3533e55fa502415221296fa307
วงณัฏฐี เงาะปก <s6002041520107@MacMini-213-03.local>
2019-03-21 14:03:27 +0700

    edit App component

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+
 import {name as appName} from './app.json';
-import App from '.component/App';
+import App from './components/App';
 
 AppRegistry.registerComponent(appName, () => App);
