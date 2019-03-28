8d2344b65680411605ac100cf029e53973189380
วงณัฏฐี เงาะปก <s6002041520107@MacMini-213-03.local>
2019-03-28 16:14:32 +0700

    Adding Style Of Button

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 //import library
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 import CardSection from './CardSection';
 
 //create component
@@ -8,13 +8,37 @@ class Card extends Component {
     render() {
         return (
             <View>
-                <CardSection />
-                <CardSection />
-                <CardSection />
+                <Text>พรุ่งนี้ค่อย</Text>
+                <Text>ป็อบ ปองกูล</Text>
+                <CardSection>
+                    <Image 
+                    style={{ width: 450, height: 450}}
+                        source={{uri: 'https://i.ytimg.com/vi/YCaGYUIfdy4/maxresdefault.jpg' }} 
+                    />
+                </ CardSection>
+
+                <CardSection>
+                    <View style={{ width:100 }}> 
+                    <Button
+                        title="Click"
+                        color="#841584"
+                    />
+                    </View>
+                </ CardSection>
             </View>
         );
     }
 }
+
+//style
+const style={
+    button: { 
+        justifyContent: 'center',
+        alignItem: 'center',
+        marginTop: 10
+    }
+}
+       
 
 // export
 export default Card;
