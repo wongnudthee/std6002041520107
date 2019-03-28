e23dcf6842e037ba31ef703c8bb14ed1d9fc8807
วงณัฏฐี เงาะปก <s6002041520107@MacMini-213-03.local>
2019-03-28 14:53:30 +0700

    decorate component with styles

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,32 @@
 //import library
 import React, { Component } from 'react';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 
 //create component
 class Header extends Component {
     render() {
-        return(
-                <View>
-                    <Text>{this.props.title}</Text>
+        return (
+                <View style={styles.header}>
+                    <Text style={styles.text}>
+                        {this.props.title}
+                    </Text>
                 </View>
-
         );
     }
 }
+
+//style
+const styles ={
+    header: {
+        padding: 10,
+        alignItems: 'center'
+    },
+    text:{
+        fontSize: 50
+    }
+}
+
+
 //export
 export default Header;
