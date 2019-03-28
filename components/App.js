3fb64612eb0246428f7c7a1e928e5a4900c1a242
วงณัฏฐี เงาะปก <s6002041520107@MacMini-213-03.local>
2019-03-28 14:28:21 +0700

    App in Component

--- components/App.js
+++ components/App.js
@@ -1,12 +1,14 @@
 //import library
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from './Header'
 //wrtie component
 class App extends React.Component {
     render() {
         return(
             //component about react native
             <View>
+                <Header title="Albums"/>
                 <Text>Hello World!!</Text>
             </View>
         );
