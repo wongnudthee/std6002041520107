2d8d8e28d22c1138cfa3ff1bcb3911a3806091be
Wongnudthee <macintoshhd@Macintoshs-MacBook-Pro.local>
2019-04-04 17:12:17 +0700

    create Login component and using

--- components/App.js
+++ components/App.js
@@ -1,23 +1,57 @@
-//import library
-import React from 'react';
-import { View, Text } from 'react-native';
-import Header from './Header';
-import Card from './Card';
+// import library
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
 
-//wrtie component
-class App extends React.Component {
+// write component
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+        // this.onChangeEmail = this.onChangeEmail.bind(this)
+    }
+    onChangeEmail(e) {
+        console.log('onChangeEmail', e)
+        this.setState({ email: e}) 
+    }
+    onChangePassword(e) {
+        this.setState({ password: e})
+    }
+    onPress() {
+        console.log(this.state)
+        const url = 'http://128.199.240.120:9999/api/auth/login'
+        axios.post(url, this.state)
+            .then(response => {
+                console.log('token ', response.data.data.token)
+            })
+    }
     render() {
-        return(
-            //component about react native
+        return (
             <View>
-                <Header title="Albums"/>
-                <Text></Text>
-                <Card />
+                <TextInput
+                    style={{ height: 40, fontWeight: 'bold', fontSize: 20 }}
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChangeText={this.onChangeEmail.bind(this)}
+                />
+                <TextInput
+                    secureTextEntry
+                    style={{ height: 40, fontWeight: 'bold', fontSize: 20 }}
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChangeText={this.onChangePassword.bind(this)}
+                />
+                <Button  
+                    title="Login"
+                    onPress={this.onPress.bind(this)}
+                />
             </View>
         );
     }
-
 }
 
-//export
-export default App;
+// export
+export default Login; 
